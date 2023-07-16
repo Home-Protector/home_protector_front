327542c1697a9b63ef0bd4cfdce2d872f49489a2
solightnsalt <>
2023-07-16 20:52:14 +0900

    Refactor: form component 분리

---
+++ home-protector/src/features/form/postInput/PostInput.jsx
@@ -0,0 +1,30 @@
+import * as S from "./style";
+
+const PostInput = () => {
+	return (
+		<S.Article>
+			<S.InputDiv>
+				<S.InputSpan fontSize="18" fontWeight="700">
+					사진을 추가해주세요.
+				</S.InputSpan>
+				<S.InputSpan fontSize="14">최대 10장까지 올릴 수 있어요.</S.InputSpan>
+				<S.Label for="input-file">PC에서 불러오기</S.Label>
+				<S.Input type="file" id="input-file" accept="image/jpg, image/png, image/gif" />
+			</S.InputDiv>
+			<S.Textarea
+				type="text"
+				height="35"
+				fontSize="30"
+				border="2px solid gray"
+				placeholder="제목을 입력해주세요."
+			/>
+			<S.Textarea type="text" height="500" fontSize="15" placeholder="내용을 입력해주세요." />
+			<S.ButtonDiv>
+				<S.Button>등록</S.Button>
+				<S.Button>취소</S.Button>
+			</S.ButtonDiv>
+		</S.Article>
+	);
+};
+
+export default PostInput;
