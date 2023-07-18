f5fd8f0c7782ef82aec6d70a5c1e4c490207d3d4
hani <ploo18800@gmail.com>
2023-07-18 17:21:08 +0900

    Fix: 토큰 디코딩 개선

--- home-protector/src/common/components/header/Header.jsx
+++ home-protector/src/common/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import * as S from "./style";
 import logo from '../../../img/logo.png'
 import { useNavigate } from "react-router-dom";
-import base64 from 'base-64';
+import jwt_decode from "jwt-decode";
 
 const Header = () => {
 	const accessToken = localStorage.getItem("accessToken"); // 사용자 토근 (token)
@@ -16,11 +16,10 @@ const Header = () => {
 	const navigate = useNavigate();
 	if (accessToken) { // 회원
 		// token 디코딩하여 저장된 정보 가져오는 내용
-		let payload = accessToken.substring(accessToken.indexOf('.')+1,accessToken.lastIndexOf('.'));
-		let dec = JSON.parse(base64.decode(payload));
+		const infoDict = jwt_decode(accessToken);
 
 		headerDiv = <S.NavWrapper>
-									<S.UserWrapper>{dec.nickname} 님</S.UserWrapper>
+									<S.UserWrapper>{infoDict.nickname} 님</S.UserWrapper>
 									<S.NavBtn onClick={() => {handleClickLogout()}}>로그아웃</S.NavBtn>
 									<S.WritingBtn to="/form">글쓰기</S.WritingBtn>
 								</S.NavWrapper>
