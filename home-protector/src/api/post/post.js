34d900b69eec2e6bafe8d7cf121a7c5dd1324787
solightnsalt <>
2023-07-18 22:55:24 +0900

    Feat: main 게시글 목록

--- home-protector/src/api/post/post.js
+++ home-protector/src/api/post/post.js
@@ -3,9 +3,14 @@ import axios from "axios";
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 // 조회
-
+export const fetchPosts = async () => {
+	const { data } = await axios.get(`${SERVER_URL}/api/post`);
+	return data;
+};
 // 등록
 export const addPost = async (newPost) => {
-    const response = await axios.post(`${SERVER_URL}/api/post`, newPost);
-    return response.data;
-}
+	const response = await axios.post(`${SERVER_URL}/api/post`, newPost);
+	return response.data;
+};
+
+//
