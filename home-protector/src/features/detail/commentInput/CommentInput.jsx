c6595d44a07e786e07489c191c1319189abbc256
solightnsalt <>
2023-07-20 00:12:54 +0900

    Feat: 로그인 시에만 댓글

--- home-protector/src/features/detail/commentInput/CommentInput.jsx
+++ home-protector/src/features/detail/commentInput/CommentInput.jsx
@@ -3,10 +3,13 @@ import { useMutation, useQueryClient } from "react-query";
 import useInput from "../../../hooks/useInput.js";
 import { useParams } from "react-router-dom";
 import { addComment } from "../../../api/post/post.js";
+import { useNavigate } from "react-router-dom";
 
 const CommentInput = () => {
 	const [comment, setComment, handleChangeComment] = useInput();
 	const { postId } = useParams();
+	const accessToken = localStorage.getItem("accessToken");
+	const navigate = useNavigate();
 
 	const queryClient = useQueryClient();
 	const addCommentMutation = useMutation(addComment, {
@@ -16,19 +19,20 @@ const CommentInput = () => {
 	});
 
 	const handleCommentSubmit = () => {
-		// 	try {
-		// 		await addCommentMutation.mutateAsync({
-		// 			postId: postId,
-		// 			newComment: {
-		// 				text: comment,
-		// 			},
-		// 		});
-		// 		console.log("comment");
-		// 		setComment("");
-		// 	} catch (error) {
-		// 		alert("오류가 발생했습니다.");
-		// 	}
-		addCommentMutation.mutate(setComment);
+		if (accessToken) {
+			try {
+				addCommentMutation.mutateAsync({
+					postId: postId,
+					newComment: comment,
+				});
+				setComment("");
+			} catch (error) {
+				alert("오류가 발생했습니다.");
+			}
+		} else {
+			alert("로그인해주세요.");
+			navigate("/login");
+		}
 	};
 
 	return (
