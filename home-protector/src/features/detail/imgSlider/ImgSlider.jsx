8dbf4a57181b675052f4521e3d1d2cb134e8375c
solightnsalt <>
2023-07-19 21:38:22 +0900

    Feat: img slider 연결

--- home-protector/src/features/detail/imgSlider/ImgSlider.jsx
+++ home-protector/src/features/detail/imgSlider/ImgSlider.jsx
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as S from "./style";
 
-const ImgSlider = () => {
+const ImgSlider = ({ images }) => {
 	const settings = {
 		dots: true,
 		fade: true,
@@ -13,14 +13,18 @@ const ImgSlider = () => {
 		arrows: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		autoPlay: true,
+		autoPlayPause: true,
+		autoPlaySpeed: 500,
+		autoPlayHover: true,
 	};
 
-	const imagesContext = require.context("../../../img/temp", false, /\.(png|jpe?g|gif|svg)$/);
-	const images = imagesContext.keys().map(imagesContext);
+	const imagesToShow = images.slice(1);
+
 	return (
 		<S.SliderWrapper>
 			<Slider {...settings}>
-				{images.map((imageUrl, index) => (
+				{imagesToShow.map((imageUrl, index) => (
 					<S.ImageWrapper key={index}>
 						<img src={imageUrl} alt="content-images" />
 					</S.ImageWrapper>
